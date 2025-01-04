885a9c71da2d468f3ea48d75a6b4619797027568
Navaneeth Santosh <navaneethsantosh74@gmail.com>
2025-01-04 11:16:07 +0530

    Create Output.sh

---
+++ Odd OR Even/Output.sh
@@ -0,0 +1,7 @@
+Enter the number to check wheteher odd or even : 
+3
+The number 3 is odd
+student@dl-14:~/navan$ ./trial.sh
+Enter the number to check wheteher odd or even : 
+2
+The number 2 is even
