191dae13d7bdc9eb3a7b4a52c77a545695b4e83f
Navaneeth Santosh <navaneethsantosh74@gmail.com>
2025-01-04 11:21:00 +0530

    Create Output.sh

---
+++ Calculator/Output.sh
@@ -0,0 +1,35 @@
+ARITHMETIC CALCULATOR OPERTIONS 
+1->ADDITION,2->DIFFERENCE,3->PRODUCT,4->DIVISION
+Enter operation to be performed : 
+1
+Enter the first and second number : 
+1
+2
+Sum is 3 
+student@dl-14:~/navan$ ./trial.sh
+ARITHMETIC CALCULATOR OPERTIONS 
+1->ADDITION,2->DIFFERENCE,3->PRODUCT,4->DIVISION
+Enter operation to be performed : 
+2
+Enter the first and second number :
+3
+2
+The difference is 1
+student@dl-14:~/navan$ ./trial.sh
+ARITHMETIC CALCULATOR OPERTIONS 
+1->ADDITION,2->DIFFERENCE,3->PRODUCT,4->DIVISION
+Enter operation to be performed : 
+3
+Enter the first and second number  : 
+3
+2
+The product is 6
+student@dl-14:~/navan$ ./trial.sh
+ARITHMETIC CALCULATOR OPERTIONS 
+1->ADDITION,2->DIFFERENCE,3->PRODUCT,4->DIVISION
+Enter operation to be performed : 
+4
+Enter the divident and divisor : 
+45
+5
+The quotient is 9
